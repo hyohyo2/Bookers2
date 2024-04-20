ca4a9fb5044502e493925afa7dddcd410798b412
EC2 Default User <ec2-user@ip-172-31-41-164.ap-northeast-1.compute.internal>
2024-04-20 10:35:52 +0000

    bookers2 ユーザー詳細テーブル作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,36 +1,45 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-  
+
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
 
   def index
     @books = Book.all
+    @book = Book.find(params[:id])
+    @user = @book.user
+
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
-    
+
   end
 
   def edit
+    @book =Book.find(params[:id])
   end
-  
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
   #投稿データのストロングパラメータ
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :caption)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,31 @@
 class UsersController < ApplicationController
   def index
     @users= User.all
-
+    @user = User.find(params[:User_id])
+    @new_book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
+    @books = @user.books
+    @new_book = Book.new
   end
 
   def edit
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
   #ユーザデータのストロングパラメータ
   private
 
   def user_params
-    params.require(:user).permit(:name, :image)
+    params.require(:user).permit(:name, :profile_image)
 
   end
 
