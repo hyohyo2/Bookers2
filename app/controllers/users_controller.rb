f46028fbcb175dab94eef35901603f3c9017731c
EC2 Default User <ec2-user@ip-172-31-41-164.ap-northeast-1.compute.internal>
2024-04-20 01:42:59 +0000

    bookers2 user一覧作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,23 @@
 class UsersController < ApplicationController
+  def index
+    @users= User.all
+
+  end
+
   def show
     @user = User.find(params[:id])
-    @books = @user.books
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   #ユーザデータのストロングパラメータ
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :image)
-    
+
   end
-  
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,5 @@ class Book < ApplicationRecord
   belongs_to :user
   
   
+  
 end
